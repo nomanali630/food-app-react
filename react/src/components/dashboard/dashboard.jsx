c18b6a0b8326f12501cbdeedac004f18fad83534
nomanali630 <hussainkhaan78@gmail.com>
2021-02-25 09:48:22 +0500

    cart

--- react/src/components/dashboard/dashboard.jsx
+++ react/src/components/dashboard/dashboard.jsx
@@ -1,9 +1,30 @@
 // import react from "react";
 
-import Nav from "../nav/nav"
+// import Nav from "../nav/nav"
 import axios from 'axios'
 import {useGlobalState} from '../../context/globalContext'
+import React, { useState } from 'react';
+import './dashboard.css';
+import Products from './Products';
+import Cart from './Cart';
+
+const PAGE_PRODUCTS = 'products';
+const PAGE_CART = 'cart';
 function Dashboard() {
+  const [cart, setCart] = useState([]);
+  const [page, setPage] = useState(PAGE_PRODUCTS);
+
+  const navigateTo = (nextPage) => {
+    setPage(nextPage);
+  };
+
+  const getCartTotal = () => {
+    return cart.reduce(
+      (sum, { quantity }) => sum + quantity,
+      0
+    );
+  };
+
   const  GlobalState= useGlobalState()
 
   function logout() {
@@ -19,10 +40,29 @@ function Dashboard() {
   }
 
   return (
-    <div logout={logout}>
-      <h1>Dashboard</h1>
-      <h1>{GlobalState.user.name}</h1>
-    </div>
+    // <div logout={logout}>
+    //   <h1>Dashboard</h1>
+    //   <h1>{GlobalState.user.name}</h1>
+    // </div>
+
+    <div className="App">
+    <header>
+      <button onClick={() => navigateTo(PAGE_CART)}>
+        Go to Cart ({getCartTotal()})
+      </button>
+
+      <button onClick={() => navigateTo(PAGE_PRODUCTS)}>
+        View Products
+      </button>
+    </header>
+    {page === PAGE_PRODUCTS && (
+      <Products cart={cart} setCart={setCart} />
+    )}
+    {page === PAGE_CART && (
+      <Cart cart={cart} setCart={setCart} />
+    )}
+  </div>
+
   )
 
 }
